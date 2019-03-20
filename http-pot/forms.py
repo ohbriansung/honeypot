f306c113172ee07214aed2224f07ef79862e1875
Jordan <jaldujaili@ucdavis.edu>
2019-03-20 15:50:15 -0700

    update to hidden

--- http-pot/forms.py
+++ http-pot/forms.py
@@ -1,6 +1,8 @@
 # pythonspot.com
 from flask import Flask, render_template, flash, request
+
 from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
+import logging
 
 # App config.
 DEBUG = True
@@ -12,7 +14,8 @@ app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
 class ReusableForm(Form):
     name = TextField('Name:', validators=[validators.required()])
     email = TextField('Email:', validators=[validators.required()])
-    hidden = TextField('Hidden:')
+    hidden = TextField('Hidden:', validators=[])
+    print(hidden)
     # password = TextField('Password:', validators=[validators.required(), validators.Length(min=3, max=35)])
 
 
@@ -24,17 +27,24 @@ def hello():
     if request.method == 'POST':
         name = request.form['name']
         email = request.form['email']
-        hidden = request.form['hidden']
+        hidden = form.data['hidden']
         print (name, " ", email, " ", hidden)
 
-        if form.validate():
-            # Save the comment here.
-            flash('Thanks for registration ' + name)
+        if hidden =="":
+            if form.validate():
+                # Save the comment here.
+                flash('Thanks for registration ' + name)
+            else:
+                flash('Error: All the form fields are required. ')
         else:
-            flash('Error: All the form fields are required. ')
+            flash('Error: You might be a bot')
+            LOG_FILENAME = 'http.log'
+            logging.basicConfig(filename=LOG_FILENAME, level=logging.INFO)
+            logging.info("--------------------\n")
 
     return render_template('hello.html', form=form)
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(port=8000, debug=True)
+
